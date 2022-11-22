f6b334d731e1f7319bc0b9041336a2d015d0a1b5
UO282365 <114696606+UO282365@users.noreply.github.com>
2022-11-23 00:19:52 +0100

    Add files via upload

---
+++ MasInfoNavegador.js
@@ -0,0 +1,5 @@
+document.write("Version: "+infoNavegador.version+"\n");
+document.write("Vendedor: "+infoNavegador.vendedor+"\n");
+document.write("Plataforma. "+infoNavegador.plataforma+"\n");
+document.write("Agente: "+infoNavegador.agente+"\n");
+document.write("Java activo: "+infoNavegador.javaActivo+"\n");
